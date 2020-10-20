8ee5247f893477749e8f6109163db6386c99fa90
shevpai <lasttrueman69@gmail.com>
2020-10-20 18:08:48 +0300

    refactor: prep for deploy

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,6 @@ import React, { FC } from 'react';
 
 export const Navbar: FC = () => (
   <nav className="navbar navbar-expand-sm navbar-dark bg-primary mb-4">
-    <div className="navbar-brand">RR-ts</div>
-    <div className="navbar-collapse" id="navbarText" />
+    <div className="navbar-brand">Search-Repo</div>
   </nav>
 );
