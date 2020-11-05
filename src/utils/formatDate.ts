be105b53e25e710e5cf6f028d55173e3a3559474
shevpai <lasttrueman69@gmail.com>
2020-11-05 15:23:50 +0200

    refactor: change data fromatter

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,20 +1,9 @@
-type numstr = number | string;
-
 export function formatDate(date: Date): string {
-  let dd: numstr = date.getDate();
-  if (dd < 10) dd = `0${dd}`;
-
-  let mm: numstr = date.getMonth() + 1;
-  if (mm < 10) mm = `0${mm}`;
-
-  let yy: numstr = date.getFullYear() % 100;
-  if (yy < 10) yy = `0${yy}`;
-
-  let hh: numstr = date.getHours();
-  if (hh < 10) hh = `0${hh}`;
-
-  let mn: numstr = date.getMinutes();
-  if (mn < 10) mn = `0${mn}`;
-
-  return `${dd}.${mm}.${yy}  ${hh}:${mn}`;
+  return new Intl.DateTimeFormat('en-US', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  }).format(new Date(date));
 }
